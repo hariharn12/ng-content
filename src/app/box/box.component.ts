575a2f27a8f25a0506936111365665961b6c7763
hariharn12 <140273143+hariharn12@users.noreply.github.com>
2024-01-27 12:14:34 +0530

    creating a ng-content

---
+++ src/app/box/box.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-box',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './box.component.html',
+  styleUrl: './box.component.css'
+})
+export class BoxComponent {
+
+}
